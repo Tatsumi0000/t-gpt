73ec92370b7fe8840cdee3dd6188c38714412b8a
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2025-04-28 00:24:22 +0900

    Base側でyamlを読み込ませて各ファイルで使い回す

--- t-gpt/base/Base.py
+++ t-gpt/base/Base.py
@@ -1,6 +1,7 @@
 import os
 from os.path import dirname, join
 
+import yaml
 from dotenv import load_dotenv
 
 
@@ -11,28 +12,39 @@ class Base(object):
     それ以外は基本的に実装しない
 
     Attributes:
-    ----------
         current_dir(str): 各コードのディレクトリ
+        dataset_yaml(list[str]): データセットの区分設定
+        datasource_yaml(dict): どこのデータソースから取得するかの設定
     """
 
     def __init__(self, file_name: str) -> None:
         """コンストラクタ。環境変数を読み込む
 
-        Parameters:
-        -----------
-        file_name(str): ファイル名
+        Args:
+            file_name(str): ファイル名
         """
         self.current_dirname = dirname(file_name)
         self.setup_env()
+        self.setup_yaml()
 
     def setup_env(self) -> None:
         """環境変数をセットアップする"""
         dotenv_path = join(self.current_dirname, "../../.env")
         load_dotenv(dotenv_path)
 
+    def setup_yaml(self) -> None:
+        """データセットの設定ファイルを読み込む"""
+        datasource_yaml_path = join(self.current_dirname, "../../config/datasource.yml")
+        with open(datasource_yaml_path, "r") as file:
+            self.datasource_yaml = yaml.safe_load(file)
+
+        dataset_yaml_path = join(self.current_dirname, "../../config/dataset.yml")
+        with open(dataset_yaml_path, "r") as file:
+            self.dataset_yaml = yaml.safe_load(file)
+
 
 def is_development() -> bool:
-    """開発モードかどうか。ローカルでの開発中で使う想定"""
+    """開発モードかどうか。ローカルでの開発中に使う想定"""
     return os.environ["ENV"] == "development"
 
 
