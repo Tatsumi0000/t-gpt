d03cc3af7861152543a355be16281c8672541f2c
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2025-04-29 03:40:31 +0900

    使用しているモデルが古かったので修正

--- t-gpt/gemini/CreateGeminiIndex.py
+++ t-gpt/gemini/CreateGeminiIndex.py
@@ -1,15 +1,12 @@
 from os.path import join
 
-from .GeminiBase import GeminiBase
-from llama_index.core import (
-    GPTVectorStoreIndex,
-    Settings,
-)
+from llama_index.core import GPTVectorStoreIndex, Settings
 from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
 from llama_index.llms.google_genai import GoogleGenAI
 
 from ..base.Base import is_development, is_production
 from ..document_reader.LoadConfluenceDocument import LoadConfluenceDocument
+from .GeminiBase import GeminiBase
 
 
 class CreateGeminiIndex(GeminiBase):
@@ -22,23 +19,10 @@ class CreateGeminiIndex(GeminiBase):
 
     def __init__(
         self,
-        llm_model: str = "models/gemini-1.5-flash",
-        embed_model: str = "models/embedding-001",
     ):
-        """コンストラクタ
-        Args:
-            llm_model(str): LLMの使用モデル
-            embed_model(str): 文書のベクトル化(embed)に使うモデル名
-        """
+        """コンストラクタ"""
         super().__init__(__file__)
-        self.llm_model = llm_model
-        self.embed_model = embed_model
         self.setup_gemini()
-
-    def setup_gemini(self) -> None:
-        """使用するGeminiのモデルを設定する"""
-        Settings.llm = GoogleGenAI(model_name=self.llm_model)
-        Settings.embed_model = GoogleGenAIEmbedding(model_name=self.embed_model)
 
     def create_and_save_index(self) -> None:
         """LLMに渡すために文書のindex化&保存。
--- t-gpt/gemini/GeminiBase.py
+++ t-gpt/gemini/GeminiBase.py
@@ -1,19 +1,40 @@
 from os.path import join
 
+from llama_index.core import Settings
+from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
+from llama_index.llms.google_genai import GoogleGenAI
+
 from ..base.Base import Base
 
 
 class GeminiBase(Base):
-    """Geminiに問い合わせする時に使う便利クラス"""
+    """Geminiに問い合わせする時に使う便利クラス
 
-    def __init__(self, file: str) -> None:
-        """
+    Parameters:
+        file(str): ファイル名
+        llm_model(str): 使用するLLMのモデル名
+        embed_model(str): index化する時に使うモデル名
+    """
+
+    def __init__(
+        self,
+        file: str,
+        llm_model: str = "models/gemini-2.5-flash",
+        embed_model: str = "text-embedding-004",
+    ) -> None:
+        """コンストラクタ
 
         Args:
             file(str): ファイル名
-
         """
         super().__init__(file)
+        self.llm_model = llm_model
+        self.embed_model = embed_model
+
+    def setup_gemini(self) -> None:
+        """使用するGeminiのモデルを設定する"""
+        Settings.llm = GoogleGenAI(model_name=self.llm_model)
+        Settings.embed_model = GoogleGenAIEmbedding(model_name=self.embed_model)
 
     def save_index_file_dir_path(self, dataset_name: str) -> str:
         """index化したデータを保存するディレクトリパスを生成。
